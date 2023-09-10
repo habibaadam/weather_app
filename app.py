14aa0de99072fa7cd3e6e69f2cf286d9786740e9
habibaadam <habiepalmer@gmail.com>
2023-09-10 09:38:19 +0000

    added module imports

---
+++ app.py
@@ -0,0 +1,23 @@
+""" Importing library for creating GUI"""
+from tkinter import *
+
+"""Alias set for library"""
+import tkinter as tt
+
+"""Library for converting address to longitude and latitude"""
+from geopy.geocoders import nominatim
+
+"""Importing module for finding timezone for locations"""
+from timezonefinder import TimezoneFinder
+
+"""Importing library for manipulating datetime objects"""
+import pytz
+
+"""Importing module for working with dates ans times"""
+from datetime import datetime
+
+"""Importing  module for making HTTP requests and getting responses"""
+import requests
+
+"""Importing module for creating dialogue boxes in GUI"""
+from tkinter import ttk, messagebox
