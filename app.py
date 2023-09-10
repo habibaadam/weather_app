9639dfcabceb7e3f7cc1783efd90c31b9dfc71a8
habibaadam <habiepalmer@gmail.com>
2023-09-10 12:13:41 +0000

    background and user interactive text box added

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 """ Importing library for creating GUI"""
+from tkinter import *
 import tkinter as tk
+
+""" Import PIL for image handling"""
+from PIL import Image, ImageTk 
 
 """Library for converting address to longitude and latitude"""
 from geopy.geocoders import Nominatim
@@ -20,10 +24,36 @@ import requests
 from tkinter import ttk, messagebox
 
 """Setting up the main window for our app"""
+
 window = tk.Tk()
+window.configure(bg="black")
 window.title("OUR WEATHER")
 window.geometry("900x500+300+200")
-window.resizable(True, True)
 
+"""Trying to set background image"""
+
+background_image = Image.open("/Users/user/Desktop/bbg.png")
+background_photo = ImageTk.PhotoImage(background_image)
+
+"""Create a label to display the background image"""
+background_label = tk.Label(window, image=background_photo)
+background_label.place(relwidth=1, relheight=1)  # Making the label cover the entire window
+
+window.resizable(False, False)
+
+"""Seach box"""
+Box_image=PhotoImage(file="/Users/user/Desktop/search.png")
+ourimage=Label(image=Box_image)
+ourimage.place(x=20,y=20)
+
+textfield=tk.Entry(window,justify="center", width=17,font=("poppins",25,"bold"), bg="#87CEEB", border=0, fg="white")
+textfield.place(x=50,y=40)
+textfield.focus()
+
+"""seach icon"""
+icon_image=tk.PhotoImage(file="/Users/user/Desktop/search_icon.png")
+our_icon_image=Button(image=icon_image, borderwidth=0, cursor="hand2", bg="#404040")
+resized_icon_image = icon_image.subsample(1)
+our_icon_image.place(x=400,y=34)
 
 window.mainloop()
